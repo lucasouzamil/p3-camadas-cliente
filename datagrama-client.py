460a2a20b84bb3eb1831645f7bf96cc78efe017d
Lucas Lima <lucasfsl@al.insper.edu.br>
2023-09-13 19:50:21 -0300

    final

--- datagrama-client.py
+++ datagrama-client.py
@@ -49,7 +49,8 @@ def main():
         print(f" - {imageR}")
         print("-"*35)
         #txBuffer = imagem em bytes!
-        pacotes = cria_pacotes(b'\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef')
+        #pacotes = cria_pacotes(b'\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef')
+        pacotes = cria_pacotes(imageR)
         
         print('Enviando byte de sacrificio')
         com1.sendData(np.asarray(b'x00'))    #enviar byte de lixo
@@ -61,21 +62,50 @@ def main():
         time.sleep(.05)
         com1.rx.clearBuffer()
 
-        print('Iniciando transmissão de pacotes')
+
+        tempo_inicial = time.time()
+        duracao_maxima = 5 
+        recebeu = False
+        transmissao = True
         i = 0
-        while i < len(pacotes['bytearray']):
+        cn=False
+        print('Iniciando transminssão de pacotes')
+        while (i < len(pacotes['bytearray'])) and transmissao:
             txBuffer = pacotes['bytearray'][i]
             print(f'Enviou --->  {txBuffer}')
             com1.sendData(np.asarray(txBuffer)) 
             time.sleep(.05)
 
-            rxBuffer, nRx = com1.getData(15)
-            time.sleep(.05)
-            print(rxBuffer)
-            int_list = [int(byte) for byte in rxBuffer]
-            if (int_list[0] == i) and int_list[-3:]==[255,255,255]:
-                i+=1
-            com1.rx.clearBuffer()
+            while (time.time() - tempo_inicial < duracao_maxima) and i == 0:
+                if com1.rx.getBufferLen() > 0:
+                    rxBuffer, nRx = com1.getData(15)
+                    time.sleep(.05)
+                    recebeu = True
+                    i += 1
+                    break
+                print(f'{time.time() - tempo_inicial}s')
+                time.sleep(0.1)
+
+            if recebeu == False:
+                print("O servidor não estava pronto para começar a transmissão.")
+                pergunta = input("Deseja iniciar a comunicação novamente? (S/N)")
+                if pergunta.lower() == 's':
+                    transmissao = True
+                    tempo_inicial = time.time()
+                else:
+                    transmissao = False
+
+            if (transmissao == True) and (i > 0):
+                if recebeu == True and cn: 
+                    rxBuffer, nRx = com1.getData(15)
+                    time.sleep(.05)
+                    print(rxBuffer)
+                    int_list = [int(byte) for byte in rxBuffer]
+                    if (int_list[0] == i) and int_list[-3:]==[255,255,255]:
+                        i+=1
+                    com1.rx.clearBuffer()
+                cn=True
+
             
         com1.disable()
         
